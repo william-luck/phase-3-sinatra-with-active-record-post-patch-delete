7647d94556e4ac3d1e1994c8396de1e791039dd7
William Luck <luck.william.h@gmail.com>
2022-09-16 16:05:16 -0300

    commit tree cae9896a35ecd8822a7b2f3d6f93c9b853d939b3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
@@ -5,6 +7,7 @@ class ApplicationController < Sinatra::Base
     games = Game.all.order(:title).limit(10)
     games.to_json
   end
+  
 
   get '/games/:id' do
     game = Game.find(params[:id])
@@ -15,5 +18,41 @@ class ApplicationController < Sinatra::Base
       } }
     })
   end
+
+  get '/reviews' do
+    "Hello World"
+  end
+
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    # Finding the review to update
+    review = Review.find(params[:id])
+    # Then I think we are updating the data in the database using the params hash
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    # Get a response with updated review as JSON
+    review.to_json
+  end
+
 
 end
